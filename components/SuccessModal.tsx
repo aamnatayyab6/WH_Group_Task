a376e52530a9f7813d2d3fcdfb51a6f0d6d2bd25
aamnatayyab6 <aamnatayyab6@yahoo.com>
2023-10-16 02:26:12 +0200

    making responsive

--- components/SuccessModal.tsx
+++ components/SuccessModal.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const SuccessModal = (props: Props) => {
   return (
     <div className="modal-overlay">
-      <div className="flex justify-center items-center bg-header-color/90 w-1/4 h-1/4 rounded-md">
+      <div className="flex justify-center items-center bg-header-color/90 md:w-1/4 md:h-1/4 w-2/3 h-1/4 rounded-md">
         <h1 className="text-white text-center justify-center font-thin text-2xl ">Successful Registration</h1>
       </div>
     </div>
